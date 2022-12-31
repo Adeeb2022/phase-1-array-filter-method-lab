e8053b6fd20f112c9d18709f4f4ac28eec85b475
Adeeb2022 <adeebsamad01@gmail.com>
2022-12-31 12:13:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Adeeb'];
+function findMatching(p,q) {
+    return p.filter(p => p.toLowerCase() === q.toLowerCase());
+};
+function fuzzyMatch(p,q) {
+    return p.filter(p => p.charAt(0) === q.charAt(0));
+};
+function matchName(p,q) {
+    return p.filter(p => p.name === q);
+};
